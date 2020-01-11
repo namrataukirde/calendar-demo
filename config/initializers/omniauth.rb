53358f8d142a92184c5f90ecacd5ea2cd9e99a7f
namrataukirde <namrata.ukirde@kiprosh.com>
2020-01-12 03:41:58 +0530

    allow calendar auth with login and service to fetch calendar events

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "1096327935695-0dsqjl1k8j6vuahpfralpkjpopnloj1f.apps.googleusercontent.com", "MCMZYkU8eJSCqOfXC7M0333d"
+  provider :google_oauth2, "1096327935695-0dsqjl1k8j6vuahpfralpkjpopnloj1f.apps.googleusercontent.com", "MCMZYkU8eJSCqOfXC7M0333d", {scope: "userinfo.email, calendar"}
 end
--- lib/services/calendar_api.rb
+++ lib/services/calendar_api.rb
@@ -1,14 +1,13 @@
 module Services
   class CalendarApi
     URL = "https://www.googleapis.com/oauth2/v4/token"
+    CALENDAR_URL = "https://www.googleapis.com/calendar/v3/calendars/primary/events"
+    # "https://www.googleapis.com/calendar/v3/users/me/calendarList"
+    # "https://www.googleapis.com/calendar/v3/calendars/primary/events"
 
     #replace User.first wuth current_user
     def post_request
-      # uri =  URI("https://www.googleapis.com/oauth2/v4/token")
-      # parsed_url = URI.parse(url)
-      # http = Net::HTTP.new(new_uri.host, new_uri.port)
-      # http.use_ssl = true
-      req = Net::HTTP::Post.new(new_uri)
+      req = post_req
 
       req.set_form_data('client_id': '1096327935695-0dsqjl1k8j6vuahpfralpkjpopnloj1f.apps.googleusercontent.com',
           'client_secret': 'MCMZYkU8eJSCqOfXC7M0333d',
@@ -21,6 +20,25 @@ module Services
       puts res.body
     end
 
+    def get_request
+      http_obj.request(request_with_headers)
+    end
+
+    def post_req
+      Net::HTTP::Post.new(new_uri)
+    end
+
+    def get_req
+      Net::HTTP::Get.new(new_calendar_uri.path)
+    end
+
+    def request_with_headers
+      request = get_req
+      request['Content-Type'] = 'application/json'
+      request['Authorization'] = "Bearer #{User.first.token}"
+      request
+    end
+
     def http_obj
       @http ||=  Net::HTTP.new(new_uri.host, new_uri.port)
       @http.use_ssl = true
@@ -31,6 +49,10 @@ module Services
 
     def new_uri
       @uri ||= URI.parse(URL)
+    end
+
+    def new_calendar_uri
+      @calendar_uri ||= URI.parse(CALENDAR_URL)
     end
   end
 end
